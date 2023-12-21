6a8f64740ff6377ba133c961a162ddfa6c707d52
Lauri Lahti <laurilahti@Lauri-MacBook-Air.local>
2023-12-22 00:51:44 +0200

    add day7 part 2

--- 2023/Day7/main.py
+++ 2023/Day7/main.py
@@ -2,9 +2,10 @@ hands = []
 input = "input.txt"
 test = "test.txt"
 
-card_values = {'A': 14, 'K': 13, 'Q': 12, 'J': 11, 'T': 10, '9': 9, '8': 8,'7': 7, '6': 6,'5': 5, '4': 4,'3': 3, '2': 2,}
+card_values = {'A': 13, 'K': 12, 'Q': 11, 'T': 10, '9': 9, '8': 8,'7': 7, '6': 6,'5': 5, '4': 4,'3': 3, '2': 2, 'J': 1}
 
 empty_hand = {key: 0 for key in card_values}
+value_to_key = {value:key for key,value in card_values.items()}
 
 hand_value = {'High card': 1, 'One pair': 2, 'Two pair': 3, 'Three of a kind': 4, 'Full house': 5, 'Four of a kind': 6, 'Five of a kind': 7}
 
@@ -22,29 +23,48 @@ def open_file_to(file,array):
 
 def calculate_value(hand):
     values = empty_hand.copy()
+    values_without_j = empty_hand.copy()
     hand_value = 1
     card_string = ''
+    j_amount = 0
+    non_j = 0
     for card in hand["cards"]:
-        values[card] += 1
+        if card == 'J':
+            j_amount += 1
+            for i in range(2,14):
+                values[value_to_key[i]] += 1
+        else:
+            non_j += 1
+            values_without_j[card] += 1
+            values[card] += 1
         card_string += f"{card_values[card]:02d}"
     pairs = set()
+    non_j_pairs = set()
     for value in values.values():
-        if value > 0:
+        if value > 1:
             if pairs.__contains__(2) and value == 2:
-                pairs.add(6)
+                pairs.add(22)
             pairs.add(value)
-    if pairs.__contains__(5):
-        hand_value = 7
-    if pairs.__contains__(4):
-        hand_value = 6
-    if pairs.__contains__(3):
-        hand_value = 4
+    for value in values_without_j.values():
+        if value > 1:
+            if non_j_pairs.__contains__(2) and value == 2:
+                non_j_pairs.add(22)
+            non_j_pairs.add(value)
     if pairs.__contains__(2):
         hand_value = 2
-    if pairs.__contains__(3) and pairs.__contains__(2):
-        hand_value = 5
-    if pairs.__contains__(6):
+    if pairs.__contains__(22) and j_amount == 0:
         hand_value = 3
+    if pairs.__contains__(3):
+        hand_value = 4
+    if pairs.__contains__(3) and pairs.__contains__(2) and not pairs.__contains__(22) :
+        hand_value = 5
+    if non_j == 4 and j_amount == 1 and non_j_pairs.__contains__(22):
+        hand_value = 5
+    if pairs.__contains__(4):
+        hand_value = 6
+    if pairs.__contains__(5):
+        hand_value = 7
+    
     check_sum = int(hand_value.__str__()+card_string)
     hand["check_sum"] = check_sum
     return check_sum
@@ -55,6 +75,6 @@ open_file_to(input,hands)
 total_sum = 0
 for i,play in enumerate(sort_hands(hands)):
     bet = play["bet"]
+    print(play)
     total_sum += int(bet) * (i+1)
 print(total_sum)
-# print(hands)
