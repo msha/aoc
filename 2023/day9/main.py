dd53f08516c1230123556b49de34facd9a1ee165
Lauri Lahti <laurilahti@Lauri-MacBook-Air.local>
2023-12-22 19:46:14 +0200

    add day9 oart 1

---
+++ 2023/day9/main.py
@@ -0,0 +1,31 @@
+input = "input.txt"
+test = "testinput.txt"
+
+def open_file_to(file):
+    array = []
+    with open(file, 'r') as file:
+        # Read each line from the file and append to the list
+        for line in file:
+            values = []
+            for value in line.split(' '):
+                values.append(value.strip())
+            array.append(values) 
+    return array
+
+def get_differences(array, accumulated_sum):
+    differences = [int(array[i]) - int(array[i-1]) for i in range(1, len(array))]
+
+    if len(differences) == 1:
+        return differences[0] + accumulated_sum
+    return get_differences(differences, differences[-1] + accumulated_sum)
+
+
+data_set = open_file_to(input)
+
+
+sum = 0
+
+for array in data_set:
+    sum += int(array[-1]) + get_differences(array, 0)
+
+print(sum)
