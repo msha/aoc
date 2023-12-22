a5abff9997f27e00025c366ac7c8982ca4945695
Lauri Lahti <laurilahti@Lauri-MacBook-Air.local>
2023-12-22 17:30:33 +0200

    add day8 part2

--- 2023/day8/main.py
+++ 2023/day8/main.py
@@ -1,10 +1,12 @@
 input = "input.txt"
 test = "testinput.txt"
+from math import gcd
+from functools import reduce
 
-def open_file(file):
+def open_file(input_file):
     instruct = ''
     array = []
-    with open(file, 'r') as file:
+    with open(input_file, 'r') as file:
         for i,line in enumerate(file):
             if i == 0:
                 instruct += line.strip()
@@ -19,15 +21,26 @@ data = open_file(input)
 
 instructions = data["instructions"].strip()
 full_map = data["map"]
-current_tile = [d for d in full_map if d['node'] == 'AAA'][0]
+current_tiles = [d for d in full_map if d['node'][2:3] == 'A']
 steps = 0
 
-while current_tile["node"].strip() != 'ZZZ':
 
-    current_instruction = instructions[steps%len(instructions)]
-    new_node = [d for d in full_map if d['node'] == current_tile[current_instruction]]
-    current_tile = new_node[0]
+ghost_intervals = []
+
+for ghost_path in current_tiles:
+    steps = 0
+    current_tile = ghost_path
+    while not (current_tile['node'].strip()[2:3] == 'Z'): 
+        current_instruction = instructions[steps%len(instructions)]
+        new_node = [d for d in full_map if d['node'] == current_tile[current_instruction]]
+        current_tile = new_node[0]
+        steps += 1
+    ghost_intervals.append(steps)
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+def find_lcm(numbers):
+    return reduce(lcm, numbers, 1)
     
-    steps += 1
-
-print(steps)
+print(find_lcm(ghost_intervals))
