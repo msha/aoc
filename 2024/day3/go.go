13b3b838aada958f12b3a081a7f58530623aba39
Lauri Lahti <1960498+msha@users.noreply.github.com>
2024-12-09 02:30:59 +0200

    add 2024/day3/part2

--- 2024/day3/go.go
+++ 2024/day3/go.go
@@ -6,7 +6,39 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
+
+func getToDont(text string) (string, int) {
+	splet := strings.SplitN(text, "don't()", 2)
+	count := countMul(splet[0])
+	if len(splet) < 2 {
+		return "", count
+	}
+	return splet[1], count
+}
+
+func getToDo(text string) (string, int) {
+	splet := strings.SplitN(text, "do()", 2)
+	if len(splet) < 2 {
+		return "", 0
+	}
+	return splet[1], 0
+}
+
+func countMul(text string) int {
+	re := regexp.MustCompile(`mul\([0-9]*,[0-9]*\)`)
+	matches := re.FindAllString(text, -1)
+	total := 0
+	for _, match := range matches {
+		re2 := regexp.MustCompile(`\d+`)
+		matches2 := re2.FindAllString(match, -1)
+		num, _ := strconv.Atoi(matches2[0])
+		num2, _ := strconv.Atoi(matches2[1])
+		total += num * num2
+	}
+	return total
+}
 
 func main() {
 	file, err := os.Open("input.txt")
@@ -21,15 +53,11 @@ func main() {
 	total := 0
 	for scanner.Scan() {
 		text := scanner.Text()
-		re := regexp.MustCompile(`mul\([0-9]*,[0-9]*\)`)
-		matches := re.FindAllString(text, -1)
-		for _, match := range matches {
-			re2 := regexp.MustCompile(`\d+`)
-			matches2 := re2.FindAllString(match, -1)
-			num, _ := strconv.Atoi(matches2[0])
-			num2, _ := strconv.Atoi(matches2[1])
-			fmt.Println(num * num2)
-			total += num * num2
+		for len(text) > 0 {
+			var val int
+			text, val = getToDont(text)
+			total += val
+			text, _ = getToDo(text)
 		}
 	}
 	fmt.Println(total)
