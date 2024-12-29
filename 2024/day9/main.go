08d071b44a21715ab894d53345a7c9e1f66cf197
Lauri Lahti <1960498+msha@users.noreply.github.com>
2024-12-29 21:23:36 +0200

    add day9/part2

--- 2024/day9/main.go
+++ 2024/day9/main.go
@@ -13,6 +13,14 @@ type block struct {
 	empty bool
 }
 
+func deepCopy(original []block) []block {
+	copy := make([]block, len(original))
+	for i := range original {
+		copy[i] = original[i]
+	}
+	return copy
+}
+
 func getBlockString(blocks []block) string {
 	output := ""
 	for _, bl := range blocks {
@@ -48,11 +56,74 @@ func organizeBlocks(blocks []block) []block {
 	return blocks
 }
 
+func findAndMoveBlocks(blocks []block, num int, end int, start int) []block {
+	size := end - start + 1
+
+	bestStart := -1
+	currentStart := -1
+	currentLength := 0
+
+	for i := 0; i < start; i++ {
+		if blocks[i].empty {
+			if currentStart == -1 {
+				currentStart = i
+			}
+			currentLength++
+			if currentLength == size {
+				bestStart = currentStart
+				break
+			}
+		} else {
+			currentStart = -1
+			currentLength = 0
+		}
+	}
+
+	if bestStart != -1 {
+		for i := 0; i < size; i++ {
+			blocks[bestStart+i].num = num
+			blocks[bestStart+i].empty = false
+		}
+		for i := start; i <= end; i++ {
+			blocks[i].num = 0
+			blocks[i].empty = true
+		}
+	}
+
+	return blocks
+}
+
+func organizeBlocks2(blocks []block) []block {
+	maxID := 99999
+	for id := maxID; id >= 0; id-- {
+		start := -1
+		end := -1
+
+		for i := 0; i < len(blocks); i++ {
+			if !blocks[i].empty && blocks[i].num == id {
+				if start == -1 {
+					start = i
+				}
+				end = i
+			} else if start != -1 {
+				blocks = findAndMoveBlocks(blocks, id, end, start)
+				start = -1
+				end = -1
+			}
+		}
+		if start != -1 {
+			blocks = findAndMoveBlocks(blocks, id, end, start)
+		}
+	}
+	return blocks
+}
+
 func calcHash(blocks []block) int {
 	total := 0
 	for i, bl := range blocks {
 		total += i * bl.num
 	}
+
 	return total
 }
 
@@ -86,11 +157,16 @@ func main() {
 			}
 		}
 	}
+	var blocks2 []block
+	blocks2 = deepCopy(blocks)
 	organizeBlocks(blocks)
 	//str := getBlockString(blocks)
 	//fmt.Println(str)
+	organizeBlocks2(blocks2)
+	//str = getBlockString(blocks2)
+	//fmt.Println(str)
 	fmt.Println("part 1:", calcHash(blocks))
-
+	fmt.Println("part 2:", calcHash(blocks2))
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Error reading file:", err)
 	}
