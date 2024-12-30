f60b554ade06db0fe0c44bb8fdee1770afefb6b9
Lauri Lahti <1960498+msha@users.noreply.github.com>
2024-12-30 03:10:35 +0200

    add day10/part1

---
+++ 2024/day10/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"container/list"
+	"fmt"
+	"os"
+)
+
+type cord struct {
+	y      int
+	x      int
+	height int
+}
+
+func getNeighbors(current cord, cords [][]rune) []cord {
+	x := current.x
+	y := current.y
+	height := current.height
+	var output []cord
+	if x > 0 {
+		nh := int(cords[y][x-1] - '0')
+		if nh-1 == height {
+			output = append(output, cord{x: x - 1, y: y, height: nh})
+		}
+	}
+	if y > 0 {
+		nh := int(cords[y-1][x] - '0')
+		if nh-1 == height {
+			output = append(output, cord{x: x, y: y - 1, height: nh})
+		}
+	}
+	if x < len(cords[0])-1 {
+		nh := int(cords[y][x+1] - '0')
+		if nh-1 == height {
+			output = append(output, cord{x: x + 1, y: y, height: nh})
+		}
+	}
+	if y < len(cords)-1 {
+		nh := int(cords[y+1][x] - '0')
+		if nh-1 == height {
+			output = append(output, cord{x: x, y: y + 1, height: nh})
+		}
+	}
+
+	return output
+}
+
+func bfs(start cord, cords [][]rune) int {
+	queue := list.New()
+	visited := make(map[cord]bool)
+
+	queue.PushBack(start)
+	visited[start] = true
+	total := 0
+	for queue.Len() > 0 {
+		element := queue.Front()
+		node := element.Value.(cord)
+		queue.Remove(element)
+		for _, neighbor := range getNeighbors(node, cords) {
+			if !visited[neighbor] {
+				if neighbor.height == 9 {
+					total++
+				}
+				queue.PushBack(neighbor)
+				visited[neighbor] = true
+			}
+		}
+	}
+
+	return total
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+	var amap [][]rune
+	var trailheads []cord
+
+	scanner := bufio.NewScanner(file)
+	y := 0
+	for scanner.Scan() {
+		var line []rune
+		row := scanner.Text()
+		for x, char := range row {
+			if char == '0' {
+				trailheads = append(trailheads, cord{x: x, y: y, height: 0})
+			}
+			line = append(line, char)
+		}
+		amap = append(amap, line)
+		y++
+	}
+	total := 0
+	for _, c := range trailheads {
+		total += bfs(c, amap)
+	}
+	fmt.Println("part1:", total)
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+}
